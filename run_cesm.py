744ee86343bc51310e46030ec9b5b9f68d02c91b
EC2 Default User <bnb32@cornell.edu>
2021-10-12 22:42:19 +0000

    get cesm setup for varying topo

--- run_cesm.py
+++ run_cesm.py
@@ -1,15 +1,18 @@
 import ecrlcesm.environment as env
+import ecrlgcm.environment
+from ecrlgcm.misc import land_year_range,min_land_year,max_land_year
+from ecrlgcm.preprocessing import interpolate_co2
 
 import os
 import argparse
 import datetime
 
 parser=argparse.ArgumentParser(description="Run CESM")
+parser.add_argument('-exp_type',default='aqua',choices=['aqua','cam','cam_land','cam_co2','moist_hs'])
+parser.add_argument('-multiplier',default=1.0,help="CO2 Multiplier")
+parser.add_argument('-co2',default=None, help="CO2 Value")
+parser.add_argument('-land_year',default=0,type=land_year_range,metavar=f'[{min_land_year}-{max_land_year}]',help="Years prior to current era in units of Ma")
 parser.add_argument('-case',default='test',type=str)
-parser.add_argument('-aqua',default=False,action='store_true')
-parser.add_argument('-cam',default=False,action='store_true')
-parser.add_argument('-cam_land',default=False,action='store_true')
-parser.add_argument('-cam_double_co2',default=False,action='store_true')
 parser.add_argument('-ntasks',default=96,type=int)
 parser.add_argument('-nthrds',default=24,type=int)
 parser.add_argument('-run',default=False,action='store_true')
@@ -42,9 +45,10 @@ def edit_namelists():
     contents.append('nhtfrq=-%s\n'%(args.out_tstep))
     contents.append('mfilt=365\n')
 
-    if args.cam_double_co2:
-        contents.append("scenario_ghg='RAMP_CO2_ONLY'\n")
-        contents.append('ramp_co2_cap=-2.0\n')
+    if args.exp_type=='cam_co2':
+        #contents.append("scenario_ghg='FIXED'\n")
+        contents.append(f"co2vmr={args.multiplier*interpolate_co2(args.land_year)}\n")
+    #    #contents.append(f'ramp_co2_cap=-{float(args.multiplier)}\n')
 
     print("**Changing namelist file: %s**"%(nl_cam_file))
     with open(nl_cam_file,'w') as f:
@@ -52,30 +56,36 @@ def edit_namelists():
     f.close()
 
 
-if args.aqua:
+if args.exp_type=='aqua':
 
     args.compset="QSC6"
     args.res="f09_f09_mg17"
 
-elif args.cam:
+elif args.exp_type=='cam':
 
     args.custom=True
     args.compset="2000_CAM60_SLND_SICE_SOCN_SROF_SGLC_SWAV"
     args.res="f19_f19_mg17"
 
-elif args.cam_land:
+elif args.exp_type=='cam_land':
 
     args.custom=True
     args.compset="2000_CAM60%SCAM_CLM50%CN_SICE_SOCN_SROF_SGLC_SWAV"
     args.res="f19_f19_mg17"
     #args.res="T42_T42"
 
-elif args.cam_double_co2:
+elif args.exp_type=='cam_co2':
 
     args.custom=True
-    args.compset="2000_CAM60%RCO2_SLND_SICE_SOCN_SROF_SGLC_SWAV"
-    args.res="f19_f19_mg17"
+    #args.compset="2000_CAM60%RCO2_SLND_SICE_SOCN_SROF_SGLC_SWAV"
+    #args.compset="2000_CAM60%SCAM_CLM50%SP_CICE%PRES_DOCN%DOM_SROF_SGLC_SWAV"
+    args.compset="2000_CAM60%SCAM_CLM50%CN_SICE_SOCN_SROF_SGLC_SWAV"
+    args.res="T42_T42"
 
+elif args.exp_type=='moist_hs':
+    args.custom=True
+    args.compset="FTJ16"
+    args.res="T42z30_T42_mg17"
 
 else:
     print("Select valid case")
@@ -94,7 +104,13 @@ change_xml_cmd='./xmlchange NTASKS=%s' %(args.ntasks)
 #change_xml_cmd+='; ./xmlchange RUN_STARTDATE=%s' %(args.start_date)
 change_xml_cmd+='; ./xmlchange STOP_N=%s' %(args.ndays)
 
-if args.rebuild:
+if args.exp_type=='cam_co2':
+    change_xml_cmd+=f'; ./xmlchange CCSM_CO2_PPMV={args.multiplier*interpolate_co2(args.land_year)}'
+    #change_xml_cmd+=f'; ./xmlchange scenario_ghg="FIXED"'
+    #change_xml_cmd+=f'; ./xmlchange ramp_co2_cap=-{float(args.multiplier)}'
+
+
+if args.rebuild or not os.path.exists(args.case):
     os.system('rm -rf %s' %(args.case))
     case_name = (args.case).split('cases')
     os.system('rm -rf %s/cases/%s' %(env.SCRATCH_DIR,case_name[1]))
@@ -110,9 +126,11 @@ else:
     os.chdir(args.case)
     os.system(change_xml_cmd)
 
-try:
-    os.system('./case.submit')
-except:
-    print("Error submitting case")
-    exit()
+
+if args.run:
+    try:
+        os.system('./case.submit')
+    except:
+        logger.error("Error submitting case")
+        exit()
 
